5c3cc25d611eb299f71f798261926b7b63edd28c
AI Kiwi <66819523+Ai-Kiwi@users.noreply.github.com>
2022-03-29 19:54:52 +1300

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -1,1 +1,7 @@
-shell.run("OneOs/startup.lua true")
+local DevMode = false
+if DevMode == true then
+    --starts with auto updates on
+    shell.run("OneOs/startup.lua false")
+else
+    shell.run("OneOs/startup.lua true")
+end
